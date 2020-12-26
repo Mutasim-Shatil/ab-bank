19d866255f3a160cf13877120524e7010d7e2f85
Mutasim-Shatil <mutasimshatil@hotmail.com>
2020-12-26 23:41:22 +0800

    complete deposit section

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-const loginBtn = document.getElementById('login');
-loginBtn.addEventListener('click', function(){
-    const loginArea = document.getElementById("login-area");
-    loginArea.style.display = 'none';
-    const transactionArea = document.getElementById("transaction-area");
-    transactionArea.style.display = 'block'
-})
+// login btn
+const loginBtn = document.getElementById("login");
+loginBtn.addEventListener("click", function () {
+  const loginArea = document.getElementById("login-area");
+  loginArea.style.display = "none";
+  const transactionArea = document.getElementById("transaction-area");
+  transactionArea.style.display = "block";
+});
+//deposit btn
+const depositBtn = document.getElementById("add-deposit");
+depositBtn.addEventListener("click", function () {
+  const depositAmount = document.getElementById("deposit-amount").value;
+  const depositNumber = parseFloat(depositAmount);
+  updateSpanText("current-deposit", depositNumber);
+  //   const currentDeposit = document.getElementById("current-deposit").innerText;
+  //   const currentDepositNumber = parseFloat(currentDeposit);
+  //   const totalDeposit = depositNumber + currentDepositNumber;
+  //   document.getElementById("current-deposit").innerText = totalDeposit;
+  //balance
+  updateSpanText("total-balance", depositNumber);
+
+  document.getElementById("deposit-amount").value = "";
+});
+function updateSpanText(id, depositNumber) {
+  const currentBalance = document.getElementById(id).innerText;
+  const totalBalanceNumber = parseFloat(currentBalance);
+  const totalBalance = depositNumber + totalBalanceNumber;
+  document.getElementById(id).innerText = totalBalance;
+}
